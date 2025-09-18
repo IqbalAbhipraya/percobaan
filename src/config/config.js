10e876a155578467e0b15bcf7a7820345088742b
Iqbal Abhipraya <abhipraya3105@gmail.com>
2025-09-18 22:40:10 +0700

    second

--- src/config/config.js
+++ src/config/config.js
@@ -16,10 +16,14 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false } // Railway often requires SSL
+    }
   }
 }
